3d355be92ab1523be7ca1a249e761655ee314942
unknown <pross2252@gmail.com>
2022-06-02 12:38:04 -0400

    added base code in js for buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,24 +6,41 @@
 // have a go back button in the top right in order to use the .replace again and go back to the main page.
 
 // Search bar
+let APIKey="AIzaSyDqV1XcJM6Vj9R_tQl-863vSYrI4O3QdvI";
+let mainURl = "./index.html"
+let searchURL = "./search.html"
 
+// On the keyup function (pressing down on enter then releasing it) it replaces the current file with the search html
+$("#searchMapInput").keyup(function(event) {
+  if (event.keyCode === 13) {
+  // 13 is the enter key
+    window.location.assign("./search.html")
+  //using assign instead of replace because it saves history so you can go back in the browser
+  }
+});
 
+let button = "backButton";
+$(button).click(function(event) {
+ window.location.assign("./index.html")
+});
+
+// initMap common 
 function initMap() {
-    var input = document.getElementById("searchMapInput");
+    let input = document.getElementById("searchMapInput");
 
-    var autocomplete = new google.maps.places.Autocomplete(input);
+    let autocomplete = new google.maps.places.Autocomplete(input);
 
     autocomplete.addListener("place_changed", function () {
-      var place = autocomplete.getPlace();
+      let place = autocomplete.getPlace();
 
-      document.getElementById("location-snap").innerHTML =
-        place.formatted_address;
+      // document.getElementById("location-snap").innerHTML =
+        //place.formatted_address;
 
-      document.getElementById("lat-span").innerHTML =
-        place.geometry.location.lat();
+      // document.getElementById("lat-span").innerHTML =
+        //place.geometry.location.lat();
 
-      document.getElementById("lon-span").innerHTML =
-        place.geometry.location.lng();
+      // document.getElementById("lon-span").innerHTML =
+        //place.geometry.location.lng();
     });
   }
   
